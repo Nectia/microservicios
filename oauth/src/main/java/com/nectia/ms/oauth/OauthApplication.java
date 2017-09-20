12acea6ff7a76ac66818944af5e653a0afa5e029
Williams Torres <wtorres@nectia.com>
2017-09-20 17:05:47 -0300

    Se agregan proyectos eureka server y oauth provider

---
+++ oauth/src/main/java/com/nectia/ms/oauth/OauthApplication.java
@@ -0,0 +1,48 @@
+package com.nectia.ms.oauth;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.security.Principal;
+
+@RestController
+@EnableResourceServer
+@EnableAuthorizationServer
+@SpringBootApplication
+public class OauthApplication extends AuthorizationServerConfigurerAdapter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OauthApplication.class);
+
+    static {
+        // for localhost testing only
+        LOG.warn("Will now disable hostname check in SSL, only to be used during development");
+        HttpsURLConnection.setDefaultHostnameVerifier((hostname, sslSession) -> true);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApplication.class, args);
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        return user;
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("acme")
+                .secret("acmesecret")
+                .authorizedGrantTypes("authorization_code", "refresh_token", "implicit", "password", "client_credentials")
+                .scopes("webshop");
+    }
+}
